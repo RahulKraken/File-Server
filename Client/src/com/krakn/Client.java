ff59df2c910e0d41319b378311eb6161455d05aa
RahulKraken <r.rahul4511@gmail.com>
2019-03-11 03:46:00 +0530

    tried to implement thread structure but failed

--- Client/src/com/krakn/Client.java
+++ Client/src/com/krakn/Client.java
@@ -1,42 +1,131 @@
 package com.krakn;
 
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Client {
-
+    private final ObjectOutputStream out;
     private Socket socket;
-    private InputStream inputStream;
-    private DataInputStream dataInputStream;
+
+//
+//    private Socket socket;
+//    private InputStream inputStream;
+//    private DataInputStream dataInputStream;
+//
+//    public Client() throws IOException {
+//        socket = new Socket("127.0.0.1", 5555);
+//        System.out.println("connected to server.");
+//        setupStreams();
+//        readMessage();
+//        closeEverything();
+//    }
+//
+//    // D:\Dev\Java\FileShare\Outputs\test1
+//
+//    private void setupStreams() throws IOException {
+//        inputStream = socket.getInputStream();
+//        dataInputStream = new DataInputStream(inputStream);
+//    }
+//
+//    private void readMessage() throws IOException {
+//        File file = new File("D:\\dev\\Java\\FileShare\\Outputs\\test.txt");
+//        FileOutputStream fileOutputStream = new FileOutputStream(file);
+//        while (dataInputStream.read() != -1) {
+//            System.out.println(dataInputStream.read());
+//            fileOutputStream.write(dataInputStream.read());
+//        }
+////        System.out.println("nyx");
+//    }
+//
+//    private void closeEverything() throws IOException {
+//        dataInputStream.close();
+//        inputStream.close();
+//        socket.close();
+//    }
+//
 
     public Client() throws IOException {
-        socket = new Socket("127.0.0.1", 5555);
-        System.out.println("connected to server.");
-        setupStreams();
-        readMessage();
+        socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket(host, 4544);
+
+        File file = new File("D:\\Tutorials\\netcampebooks\\CCNA_Prep_Guide.pdf");
+        out = new ObjectOutputStream(socket.getOutputStream());
+        out.flush();
+        // Get the size of the file
+
+        // this part will be handled by the resolvePackets method
+        resolvePackets(file.getPath());
+
+        // this block will go inside sendData packet block
+//        int count;
+//        while ((count = in.read(bytes)) > 0) {
+//            out.write(bytes, 0, count);
+//        }
+
+        // this will go inside the closeEverything method
         closeEverything();
+//        out.close();
+////        in.close();
+//        socket.close();
     }
 
-    // D:\Dev\Java\FileShare\Outputs\test1
-
-    private void setupStreams() throws IOException {
-        inputStream = socket.getInputStream();
-        dataInputStream = new DataInputStream(inputStream);
-    }
-
-    private void readMessage() throws IOException {
-        File file = new File("D:\\dev\\Java\\FileShare\\Outputs\\test.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        while (dataInputStream.read() != -1) {
-            System.out.println(dataInputStream.read());
-            fileOutputStream.write(dataInputStream.read());
+    private synchronized void sendPacket(Packet packet) {
+        try {
+            out.writeObject(packet);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error sending some packets.");
         }
-//        System.out.println("nyx");
     }
 
-    private void closeEverything() throws IOException {
-        dataInputStream.close();
-        inputStream.close();
-        socket.close();
+    private void closeEverything() {
+        try {
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error closing something.");
+        }
+    }
+
+    public void resolvePackets(String path) {
+        File file = new File(path);
+
+        int size = 4 * 1024;
+
+        byte[] bytes = new byte[size];
+        int bytecount = 0, j = 0;
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            while ((bytecount = fileInputStream.read(bytes)) > -1) {
+
+//                File output = new File("D:\\Dev\\Java\\FileShare\\Outputs\\test" + j + ".txt");
+//                FileOutputStream outputStream = new FileOutputStream(output);
+
+//                System.out.println(Arrays.toString(bytes));
+//                for (int i = 0; i < bytecount; i++) {
+//                    System.out.print(bytes[i]);
+//                }
+                Packet packet = new Packet(size, "test" + j);
+                packet.setBytes(bytes);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendPacket(packet);
+                    }
+                }).start();
+//                System.out.println(Arrays.toString(bytes));
+                System.out.println("Sent file " + j + " - with byteCount - " + bytecount);
+                j += 1;
+//                outputStream.write(bytes);
+//                outputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/com/krakn/Server.java
+++ Server/src/com/krakn/Server.java
@@ -6,66 +6,123 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Server {
+    private Packet packet;
 
-    private Socket socket;
-    private DataOutputStream dataOutputStream;
-    private OutputStream outputStream;
+//
+//    private Socket socket;
+//    private DataOutputStream dataOutputStream;
+//    private OutputStream outputStream;
+//
+//    public Server() throws IOException {
+//        ServerSocket serverSocket = new ServerSocket(5555, 10);
+//        socket = serverSocket.accept();
+//        System.out.println(socket.getInetAddress().getHostName());
+//        setupStreams();
+//        sendMessage();
+//        closeEverything();
+//    }
+//
+//    private void setupStreams() throws IOException {
+//        outputStream = socket.getOutputStream();
+//        dataOutputStream = new DataOutputStream(outputStream);
+//    }
+//
+//    // D:\Tutorials\netcampebooks\First_PHP_MySQL.pdf
+//
+//    private void sendMessage() throws IOException {
+//        File file = new File("D:\\Google Drive\\PDF\\Programming books\\pandas.pdf");
+//        FileInputStream fileInputStream = new FileInputStream(file);
+//        byte[] bytes = new byte[2048];
+//        int bit = 0;
+//        while ((bit = fileInputStream.read(bytes)) > 0) {
+//            System.out.println(bit);
+//            dataOutputStream.write(bytes, 0, bit);
+//        }
+//    }
+//
+//    private void closeEverything() throws IOException {
+//        dataOutputStream.close();
+//        outputStream.close();
+//        socket.close();
+//    }
+//
+//    private void resolvePackets(String path) {
+//        File file = new File(path);
+//
+//        byte[] bytes = new byte[2048];
+//        int bytecount = 0, j = 0;
+//        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+//            while ((bytecount = fileInputStream.read(bytes)) > -1) {
+//
+//                File output = new File("E:\\Movies\\Parts\\output" + j + ".txt");
+//                FileOutputStream outputStream = new FileOutputStream(output);
+//
+//                System.out.println(Arrays.toString(bytes));
+//                for (int i = 0; i < bytecount; i++) {
+//                    System.out.print(bytes[i]);
+//                }
+//                j += 1;
+//                outputStream.write(bytes);
+//                outputStream.close();
+//                System.out.println("Written file " + j);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
 
-    public Server() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5555, 10);
-        socket = serverSocket.accept();
-        System.out.println(socket.getInetAddress().getHostName());
-        setupStreams();
-        sendMessage();
-        closeEverything();
-    }
+    public Server() throws IOException, FileNotFoundException, ClassNotFoundException {
+        ServerSocket serverSocket = null;
 
-    private void setupStreams() throws IOException {
-        outputStream = socket.getOutputStream();
-        dataOutputStream = new DataOutputStream(outputStream);
-    }
-
-    // D:\Tutorials\netcampebooks\First_PHP_MySQL.pdf
-
-    private void sendMessage() throws IOException {
-        File file = new File("D:\\Google Drive\\PDF\\Programming books\\pandas.pdf");
-        FileInputStream fileInputStream = new FileInputStream(file);
-        byte[] bytes = new byte[2048];
-        int bit = 0;
-        while ((bit = fileInputStream.read(bytes)) > 0) {
-            System.out.println(bit);
-            dataOutputStream.write(bytes, 0, bit);
+        try {
+            serverSocket = new ServerSocket(4544);
+        } catch (IOException ex) {
+            System.out.println("Can't setup server on this port number. ");
         }
-    }
 
-    private void closeEverything() throws IOException {
-        dataOutputStream.close();
-        outputStream.close();
-        socket.close();
-    }
+        ObjectInputStream in = null;
+        OutputStream out = null;
 
-    private void resolvePackets(String path) {
-        File file = new File(path);
+        try {
+            final Socket socket = serverSocket.accept();
+            in = new ObjectInputStream(socket.getInputStream());
 
-        byte[] bytes = new byte[2048];
-        int bytecount = 0, j = 0;
-        try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            while ((bytecount = fileInputStream.read(bytes)) > -1) {
 
-                File output = new File("E:\\Movies\\Parts\\output" + j + ".txt");
-                FileOutputStream outputStream = new FileOutputStream(output);
+//        byte[] bytes;
 
-                System.out.println(Arrays.toString(bytes));
-                for (int i = 0; i < bytecount; i++) {
-                    System.out.print(bytes[i]);
+        int j = 0;
+        while (j < 1592) {
+//            try {
+//                packet = (Packet) in.readObject();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                break;
+//            }
+//            if (packet == null) {
+//                break;
+//            }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    new Thread(new FileInputHandler(socket)).start();
                 }
-                j += 1;
-                outputStream.write(bytes);
-                outputStream.close();
-                System.out.println("Written file " + j);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            }).start();
+//            out = new FileOutputStream("D:\\Dev\\Java\\FileShare\\Outputs\\" + packet.getName() + ".txt");
+//            out.flush();
+//            System.out.println(Arrays.toString(packet.getBytes()));
+//            System.out.println(packet.getBytes().length + " - " + j);
+//            out.write(packet.getBytes());
+            j++;
         }
+        } catch (IOException ex) {
+            System.out.println("Can't get socket input stream.");
+        }
+
+//        out.close();
+//        in.close();
+//        socket.close();
+//        serverSocket.close();
     }
 }
